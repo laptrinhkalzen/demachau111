d1af8138bbe01fc55ee1499a4f26a88b5dfef709
laptrinhkalzen <68837856+laptrinhkalzen@users.noreply.github.com>
2020-10-12 08:48:23 +0700

    1

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -48,11 +48,11 @@ class FrontendController extends Controller {
         $houseware = $this->productRepo->getHousewareProduct($limit = 8);
         $news_arr = $this->newsRepo->getAllNews($limit = 7);
         $category = $this->categoryRepo->getCategory();
-        $attribute = $this->productAttributeRepo->getCategory();
+        // $attribute = $this->productAttributeRepo->getSize();
         if (config('global.device') != 'pc') {
-            return view('mobile/home/index', compact('total','hot_products_slide','product_new', 'product_hl', 'industry', 'nature', 'sale', 'plastic', 'news_arr','slide1','slide2','banner_mid', 'banner_left', 'banner_right', 'banner_end','product_all','outdoor','decor','houseware','category','attribute'));
+            return view('mobile/home/index', compact('total','hot_products_slide','product_new', 'product_hl', 'industry', 'nature', 'sale', 'plastic', 'news_arr','slide1','slide2','banner_mid', 'banner_left', 'banner_right', 'banner_end','product_all','outdoor','decor','houseware','category'));
         } else {
-            return view('frontend/home/index', compact('total','hot_products_slide','product_new', 'product_hl', 'industry', 'nature' , 'sale', 'plastic', 'news_arr','slide1','slide2','banner_mid', 'banner_left', 'banner_right', 'banner_end','product_all','outdoor','decor','houseware','category','attribute'));
+            return view('frontend/home/index', compact('total','hot_products_slide','product_new', 'product_hl', 'industry', 'nature' , 'sale', 'plastic', 'news_arr','slide1','slide2','banner_mid', 'banner_left', 'banner_right', 'banner_end','product_all','outdoor','decor','houseware','category'));
         }
     }
     
