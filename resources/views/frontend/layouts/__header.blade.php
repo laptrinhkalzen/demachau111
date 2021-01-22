1cec306611575ccb10fbe0d5508f5157605366ef
laptrinhkalzen <tiendat98274@gmail.com>
2021-01-22 10:53:40 +0700

    dat 22/1

--- resources/views/frontend/layouts/__header.blade.php
+++ resources/views/frontend/layouts/__header.blade.php
@@ -10,6 +10,8 @@
     <title>Eshop - eCommerce HTML5 Template.</title>
     <!-- Favicon -->
     <link rel="icon" type="image/png" href="images\favicon.png">
+    <!-- Web Font -->
+    <link href="https://fonts.googleapis.com/css?family=Poppins:200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
   
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
