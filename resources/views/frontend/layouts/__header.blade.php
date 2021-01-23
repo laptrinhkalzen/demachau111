a367ee41877cf1c507b012e35d5f8ff4e69de29d
laptrinhkalzen <tiendat98274@gmail.com>
2021-01-23 16:14:36 +0700

    1

--- resources/views/frontend/layouts/__header.blade.php
+++ resources/views/frontend/layouts/__header.blade.php
@@ -9,7 +9,7 @@
     <!-- Title Tag  -->
     <title>Đệm á châu.</title>
     <!-- Favicon -->
-    <link rel="icon" type="image/png" href="images\favicon.png">
+    <link rel="shortcut icon" type="image / png" href="{!!$share_config->favicon!!}"/>
     <!-- Web Font -->
     <link href="https://fonts.googleapis.com/css?family=Poppins:200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
   
