27add81f003aaca61dd7f26fa4bf4d1de167c097
laptrinhkalzen <tiendat98274@gmail.com>
2021-01-23 19:00:10 +0700

    1

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -44,7 +44,7 @@ class FrontendController extends Controller {
                 $value->parent_name=DB::table('attribute')->where('id',$value->parent_id)->pluck('title');
             }
         $product_sale=DB::table('flashsale')->join('flash_sale_product','flash_sale_product.flash_sale_id','=','flashsale.id')->where('flashsale.start','<', Carbon::now('Asia/Ho_Chi_Minh'))->where('flashsale.end','>',Carbon::now('Asia/Ho_Chi_Minh'))->get();  
-        dd($product_sale);
+
           $product_attrs=$product_attributes->groupBy('product_id');
        //dd($product_danh_muc_cha);
      
