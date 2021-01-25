f28cf2c9d777bb304c3692cee32f747ef50a88bd
laptrinhkalzen <tiendat98274@gmail.com>
2021-01-25 17:16:26 +0700

    125/1

--- app/Http/Controllers/Frontend/CheckoutOrderController.php
+++ app/Http/Controllers/Frontend/CheckoutOrderController.php
@@ -29,6 +29,7 @@ class CheckoutOrderController extends Controller {
      public function buy_now($id,Request $request) {
            $id=$id;
            $option_number=$request->option_number;
+           //$option_detail=DB::table('option_detail')->where('option_id',$option_number)->get();
            $cart = session()->get('cart');
            $product=Product::find($id);
            $price=DB::table('option_detail')->where('product_id',$id)->where('option_id',$option_number)->pluck('option_price')->first();
@@ -109,10 +110,9 @@ class CheckoutOrderController extends Controller {
     }
 
     public function checkout_payment(Request $request){
-
+        
         $data=$request->except('_token');
         $data['created_at']=Carbon::now('Asia/Ho_Chi_Minh');
-        $input=array();
         $count=0;
         $sub_total=0;
         $total=0;
@@ -137,85 +137,85 @@ class CheckoutOrderController extends Controller {
             }
             $data['total']=$total;
             $data['sub_total']=$sub_total;
-           
-                $order_id=DB::table('order')->insertGetId($data);
-                    if($cart){
-                        foreach ($cart as $val) {
-                            $input['order_id'] = $order_id;
-                            $input['product_id'] = $val['product_id'];
-                            $input['option_id'] = $val['option_number'];
-                            $input['quantity'] = $val['quantity'];
-                            $input['price'] = $val['price'];
-                            $input['each_total'] = $val['quantity'] * $val['price'];
-                            $input['created_at'] =  Carbon::now('Asia/Ho_Chi_Minh');
+            $order_id=DB::table('order')->insertGetId($data);
+          
+            foreach ($cart as $val) {
+                $input['order_id'] = $order_id;
+                $input['product_id'] = $val['product_id'];
+                $input['option_id'] = $val['option_number'];
+                $input['quantity'] = $val['quantity'];
+                $input['price'] = $val['price'];
+                $input['each_total'] = $val['quantity'] * $val['price'];
+                $input['created_at'] =  Carbon::now('Asia/Ho_Chi_Minh');
+                DB::table('order_detail')->insert($input);
+            }
+            
 
-                            DB::table('order_detail')->insert($input);
+                
+            $order=DB::table('order')->where('id',$order_id)->first();
+            session()->forget('cart');
+            if($data['payment_method']==0 || $data['payment_method']==1){
+                return redirect()->route('home.index')->with('order_success','Mua hàng thành công');
+            }
+            else{
+                session(['cost_id' => $order_id]);
+                session(['url_prev' => url()->previous()]);
+                $vnp_TmnCode = "UDOPNWS1"; //Mã website tại VNPAY 
+                $vnp_HashSecret = "EBAHADUGCOEWYXCMYZRMTMLSHGKNRPBN"; //Chuỗi bí mật
+                $vnp_Url = "http://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
+                $vnp_Returnurl = "http://demachau.local";
+                $vnp_TxnRef = date("YmdHis"); //Mã đơn hàng. Trong thực tế Merchant cần insert đơn hàng vào DB và gửi mã này sang VNPAY
+                $vnp_OrderInfo = "Thanh toán hóa đơn phí dich vụ";
+                $vnp_OrderType = 'billpayment';
+                $vnp_Amount = $order->total * 100;
+                $vnp_Locale = 'vn';
+                $vnp_IpAddr = request()->ip();
+
+                $inputData = array(
+                    "vnp_Version" => "2.0.0",
+                    "vnp_TmnCode" => $vnp_TmnCode,
+                    "vnp_Amount" => $vnp_Amount,
+                    "vnp_Command" => "pay",
+                    "vnp_CreateDate" => date('YmdHis'),
+                    "vnp_CurrCode" => "VND",
+                    "vnp_IpAddr" => $vnp_IpAddr,
+                    "vnp_Locale" => $vnp_Locale,
+                    "vnp_OrderInfo" => $vnp_OrderInfo,
+                    "vnp_OrderType" => $vnp_OrderType,
+                    "vnp_ReturnUrl" => $vnp_Returnurl,
+                    "vnp_TxnRef" => $vnp_TxnRef,
+                );
+
+                if (isset($vnp_BankCode) && $vnp_BankCode != "") {
+                    $inputData['vnp_BankCode'] = $vnp_BankCode;
+                }
+                ksort($inputData);
+                $query = "";
+                $i = 0;
+                $hashdata = "";
+                foreach ($inputData as $key => $value) {
+                    if ($i == 1) {
+                        $hashdata .= '&' . $key . "=" . $value;
+                    } else {
+                        $hashdata .= $key . "=" . $value;
+                        $i = 1;
                     }
+                    $query .= urlencode($key) . "=" . urlencode($value) . '&';
                 }
 
+                $vnp_Url = $vnp_Url . "?" . $query;
+                if (isset($vnp_HashSecret)) {
+                   // $vnpSecureHash = md5($vnp_HashSecret . $hashdata);
+                    $vnpSecureHash = hash('sha256', $vnp_HashSecret . $hashdata);
+                    $vnp_Url .= 'vnp_SecureHashType=SHA256&vnp_SecureHash=' . $vnpSecureHash;
+                }
+                return redirect($vnp_Url);
 
-                $order=DB::table('order')->where('id',$order_id)->first();
-                session()->forget('cart');
-                return redirect()->route('home.index')->with('order_success','Mua hàng thành công');
-                  
-                    
-
-
-}
+            }
+        }
                            
             
-  // session(['cost_id' => $order_id]);
-                    // session(['url_prev' => url()->previous()]);
-                    // $vnp_TmnCode = "UDOPNWS1"; //Mã website tại VNPAY 
-                    // $vnp_HashSecret = "EBAHADUGCOEWYXCMYZRMTMLSHGKNRPBN"; //Chuỗi bí mật
-                    // $vnp_Url = "http://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
-                    // $vnp_Returnurl = "http://demachau.local";
-                    // $vnp_TxnRef = date("YmdHis"); //Mã đơn hàng. Trong thực tế Merchant cần insert đơn hàng vào DB và gửi mã này sang VNPAY
-                    // $vnp_OrderInfo = "Thanh toán hóa đơn phí dich vụ";
-                    // $vnp_OrderType = 'billpayment';
-                    // $vnp_Amount = $order->total * 100;
-                    // $vnp_Locale = 'vn';
-                    // $vnp_IpAddr = request()->ip();
 
-        // $inputData = array(
-        //     "vnp_Version" => "2.0.0",
-        //     "vnp_TmnCode" => $vnp_TmnCode,
-        //     "vnp_Amount" => $vnp_Amount,
-        //     "vnp_Command" => "pay",
-        //     "vnp_CreateDate" => date('YmdHis'),
-        //     "vnp_CurrCode" => "VND",
-        //     "vnp_IpAddr" => $vnp_IpAddr,
-        //     "vnp_Locale" => $vnp_Locale,
-        //     "vnp_OrderInfo" => $vnp_OrderInfo,
-        //     "vnp_OrderType" => $vnp_OrderType,
-        //     "vnp_ReturnUrl" => $vnp_Returnurl,
-        //     "vnp_TxnRef" => $vnp_TxnRef,
-        // );
-
-        // if (isset($vnp_BankCode) && $vnp_BankCode != "") {
-        //     $inputData['vnp_BankCode'] = $vnp_BankCode;
-        // }
-        // ksort($inputData);
-        // $query = "";
-        // $i = 0;
-        // $hashdata = "";
-        // foreach ($inputData as $key => $value) {
-        //     if ($i == 1) {
-        //         $hashdata .= '&' . $key . "=" . $value;
-        //     } else {
-        //         $hashdata .= $key . "=" . $value;
-        //         $i = 1;
-        //     }
-        //     $query .= urlencode($key) . "=" . urlencode($value) . '&';
-        // }
-
-        // $vnp_Url = $vnp_Url . "?" . $query;
-        // if (isset($vnp_HashSecret)) {
-        //    // $vnpSecureHash = md5($vnp_HashSecret . $hashdata);
-        //     $vnpSecureHash = hash('sha256', $vnp_HashSecret . $hashdata);
-        //     $vnp_Url .= 'vnp_SecureHashType=SHA256&vnp_SecureHash=' . $vnpSecureHash;
-        // }
-        // return redirect($vnp_Url);
     
        
     
