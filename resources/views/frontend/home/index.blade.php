334c0f69aa759a7493e44a4bb8abe8d5b1823b15
laptrinhkalzen <info@kalzen.com>
2021-02-07 22:55:20 +0700

    Minh

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -204,7 +204,7 @@ margin-top: 0px;
                 <div class="container" style="padding-left: 15px;background-color: #c70000;color: white;height: 100%;">  
 
                     <div class="row" style="margin-right: 0px;">
-                        <span style="font-size: 20px;margin:auto;"><img src="/icon/coupon.svg" style="width: 30px;object-fit: cover;margin-right: 3px;">FLASH SALE:
+                        <span style="font-size: 20px;margin:auto;"><img src="/icon/coupon.svg" style="width: 40px;object-fit: cover;margin-right: 3px;">FLASH SALE:
 
                                     <strong id="day_mb" style="background-color: black;color: white;border-radius: 5px;padding:3px;"></strong>&nbspngày
                                     <strong id="hour_mb" style="background-color: black;color: white;border-radius: 5px;padding:3px;"></strong><img src="/icon/two-dots.svg" style="width: 10px;object-fit: cover;margin-left: 3px;">
