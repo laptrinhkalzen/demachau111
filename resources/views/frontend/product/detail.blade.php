91d7eeb57e64e45ce68e85252c7e5ce15f95130e
laptrinhkalzen <tiendat98274@gmail.com>
2021-02-22 14:39:10 +0700

    luong

--- resources/views/frontend/product/detail.blade.php
+++ resources/views/frontend/product/detail.blade.php
@@ -783,7 +783,8 @@ input[type="number"] {
                                                 @endif
                                                 @endforeach
                                                 @if($dem==0)
-                                                <span style="color:red;">{{number_format($val['price'])}}đ</span>
+                                                <!-- <span style="color:red;">{{number_format($val['price'])}}đ</span> -->
+                                                <span style="color:red;">Liên hệ</span>
                                                 @endif
                                                 @else
                                                  <span style="color:red;">Liên hệ</span>
