3b176cf66793de55ef66d640ca07a281c5464f21
laptrinhkalzen <tiendat98274@gmail.com>
2021-03-01 15:08:28 +0700

    d

--- app/Http/Controllers/Api/FrontendController.php
+++ app/Http/Controllers/Api/FrontendController.php
@@ -116,7 +116,7 @@ class FrontendController extends Controller {
             $returnData['Message'] = 'Unknow error';
         }
         //Trả lại VNPAY theo định dạng JSON
-        echo json_encode($returnData, $status, ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'], JSON_UNESCAPED_UNICODE);
+        return response()->json($returnData, $status, ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'], JSON_UNESCAPED_UNICODE);
       }
 
       public function tra_gop(Request $request) {
